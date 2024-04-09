71ae51d3c16809c9021b546e81ab2301ab822e4e
Dmitrii <93027436+dmitrykosintsev@users.noreply.github.com>
2024-04-09 12:08:56 +0000

    Creating a new script to work with NASA API

---
+++ API2024/on_this_day.py
@@ -0,0 +1,7 @@
+"""The script prompts the user to choose the date in the present and 
+provides information  about the weather on Mars on the same day a few years ago."""
+import requests
+import datetime
+import json
+
+# In progress
